c13b4d99679b9ec32863645476bf4406993362a4
xnrx <rst@fd4d.ru>
2016-01-26 20:38:42 +0300

    Just stop reading these for a while, ok..

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,16 +1,16 @@
 import XMonad
 
 import XMonad.Hooks.SetWMName
-import XMonad.Layout.Spacing 
+--import XMonad.Layout.Spacing 
 main = xmonad defaultConfig
         { modMask               = mod4Mask --Use Super instead of Alt
           , terminal            = "urxvt"
           , workspaces          = myWorkspaces
           , startupHook         = setWMName "LG3D"
           , borderWidth         = 2
-          , normalBorderColor   = "none"
-          , focusedBorderColor  = "#353b3e"
-          , layoutHook = spacing 2 $ Tall 1 (50/100) (1/2)
+          , normalBorderColor   = "#353b3e"
+          , focusedBorderColor  = "lightgrey"
+          --, layoutHook = spacing 2 $ Tall 1 (50/100) (1/2)
         }
 
 myWorkspaces :: [String]
