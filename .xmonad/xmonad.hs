739d05007a5185b3d862a22992e82e116bc43b12
xnrx <rst@fd4d.ru>
2016-01-26 17:09:23 +0300

    some sublime changes
    add gtk2/3 font settings

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,15 +1,16 @@
 import XMonad
 
 import XMonad.Hooks.SetWMName
- 
+import XMonad.Layout.Spacing 
 main = xmonad defaultConfig
-        { modMask = mod4Mask -- Use Super instead of Alt
-          , terminal = "urxvt"
-          , workspaces = myWorkspaces
-          , startupHook           = setWMName "LG3D"
-          , borderWidth = 2
-          , normalBorderColor  = "#111111"
-          , focusedBorderColor = "#353b3e"
+        { modMask               = mod4Mask --Use Super instead of Alt
+          , terminal            = "urxvt"
+          , workspaces          = myWorkspaces
+          , startupHook         = setWMName "LG3D"
+          , borderWidth         = 2
+          , normalBorderColor   = "none"
+          , focusedBorderColor  = "#353b3e"
+          , layoutHook = spacing 2 $ Tall 1 (50/100) (1/2)
         }
 
 myWorkspaces :: [String]
