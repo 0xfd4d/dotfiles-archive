84c9200b99f79d9f20dcb68713118adf7558d678
xnrx <rst@fd4d.ru>
2016-02-02 02:14:24 +0300

    some topbar changes, but first workspace still not work

--- .xmonad/xmobartop.hs
+++ .xmonad/xmobartop.hs
@@ -1,11 +1,8 @@
 Config { 
-    font = "xft:Droid Sans:size=10:regular:antialias=true",
+    font = "xft:Droid Sans:size=10",
     bgColor = "#0A0F14",
     fgColor = "#98D1CE",
     position = Static { xpos = 0 , ypos = 0, width = 1366, height = 20 },
-    commands = [ Run Cpu ["-S", "True", "-t", "CPU: <total>", "-L","5","-H","40","--normal","#D26939","--high","#C33027"] 10, 
-                Run Memory ["-S", "True",  "-t","RAM: <usedratio> (<used>MiB of <total>MiB)"] 10,
-                Run XMonadLog
-                ], 
-     template = "%cpu%   %memory%   %XMonadLog%"
+    commands = [ Run StdinReader ], 
+     template = "%StdinReader%"
    }
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,61 +1,60 @@
 import XMonad
+
 import XMonad.Hooks.ManageDocks
-import XMonad.Layout.NoBorders
-import XMonad.Layout.Tabbed
-import XMonad.Layout.StackTile
 import XMonad.Hooks.DynamicLog
-import XMonad.Util.EZConfig
-import XMonad.Operations
 import XMonad.Hooks.SetWMName
+
+import XMonad.Operations
+
 import XMonad.Layout.Fullscreen
-import XMonad.Layout.PerWorkspace (onWorkspace)
-import XMonad.Util.Run(spawnPipe)
-import qualified  XMonad.StackSet as W
+
+import XMonad.Util.EZConfig
+import XMonad.Util.Run
+
+--import qualified  XMonad.StackSet as W
 import qualified Data.Map as M
 
-myWorkspaces = ["1:web","2:term","3:dev"] ++ map show [4..9]
+import System.IO
 
-myLayouts = ( avoidStruts $ smartBorders $ 
-              Tall 1 (3/100) (1/2) |||
-              StackTile 1 (3/100) (1/2) ||| 
-              simpleTabbed )
+myWorkspaces = ["www","term","dev"] ++ map show [4..9]
 
---myManageHook = composeAll [
-  --className =? "Gtk-recordmydesktop" --> doFloat,
-  --className =? "Xmessage"            --> doFloat,
-  --className =? "Gxmessage"           --> doFloat,
-  --className =? "Galculator"          --> doFloat, 
-  --className =? "Gksu"                --> doFloat 
---  ]
+myLayouts = tiled ||| Mirror tiled ||| Full
+                where
+                    tiled = Tall (1) (3/100) (1/2)
 
-myLog = dynamicLogString xmobarPP {
-  ppCurrent         = xmobarColor "#33859D" "" . wrap "[" "]",
-  ppTitle           = xmobarColor "#98D1CE" "" . wrap "[" "]",
-  ppHidden          = xmobarColor "#D26939" "",
-  ppHiddenNoWindows = xmobarColor "#98D1CE" ""
-  , ppLayout        = xmobarColor "#33859D" "" 
-}
+
+myManageHook = composeAll [
+    className =? "URxvt" --> doShift "term",
+    className =? "Subl3" --> doShift "dev"
+    ]
 
 myKeys x = M.union (keys defaultConfig x) (keysToAdd x) 
-  where
-    keysToAdd = \c -> mkKeymap c $ [
-      ("<XF86AudioRaiseVolume>", spawn "ponymix increase 2"),
-      ("<XF86AudioLowerVolume>", spawn "ponymix decrease 2")
-      ]
+            where
+                keysToAdd = \c -> mkKeymap c $ [
+                    ("<XF86AudioRaiseVolume>", spawn "ponymix increase 2"),
+                    ("<XF86AudioLowerVolume>", spawn "ponymix decrease 2")
+                    ]
 
 main = do
-  xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmonad/xmobartop.hs"
-  xmonad $ defaultConfig {
-    modMask            = mod4Mask,
-    terminal           = "urxvt",
-    borderWidth        = 2,
-    normalBorderColor  = "#081F2D",
-    focusedBorderColor = "#D26939",
-    workspaces         = myWorkspaces,
-    layoutHook         = myLayouts,
-    logHook            = myLog >>= xmonadPropLog,
-    manageHook         = manageHook defaultConfig <+> manageDocks, 
-    keys               = myKeys,
-    handleEventHook    = fullscreenEventHook,
-    startupHook        = setWMName "LG3D" }
+    xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmonad/xmobartop.hs"
+    xmonad $ defaultConfig {
+        modMask             = mod4Mask,
+        terminal            = "urxvt",
+        borderWidth         = 1,
+        normalBorderColor   = "#081F2D",
+        focusedBorderColor  = "#D26939",
+        workspaces          = myWorkspaces,
+        layoutHook          = avoidStruts $ myLayouts,
+        manageHook          = myManageHook <+> manageHook defaultConfig,
+        keys                = myKeys,
+        startupHook         = setWMName "LG3D",
+        logHook             = dynamicLogWithPP xmobarPP {
+                                ppOutput            = hPutStrLn xmproc,
+                                ppCurrent           = xmobarColor "#D26939" "" . wrap "[" "]",
+                                ppTitle             = xmobarColor "#98D1CE" "" . wrap "[" "]",
+                                ppHidden            = xmobarColor "#33859D" "",
+                                ppHiddenNoWindows   = xmobarColor "#98D1CE" "",
+                                ppLayout            = const "" 
+                                }
+    }
     
